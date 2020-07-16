6dc60292981642d84a264362a11c4ce773de87cf
ousmane dia <ousmanedia846@gmail.com>
2020-07-16 12:17:58 +0000

    fix

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -12,13 +12,26 @@ class ContactController extends AbstractController
     /**
      * @Route("/contactez-nous", name="contact")
      */
-    public function index(Request $request)
+    public function index(Request $request, \Swift_Mailer $mailer)
     {
       $form = $this->createForm(ContactType::class);
       $form->handleRequest($request);
       if ($form->isSubmitted() && $form->isValid()) {
         $contact=$form->getData();
-        //dd($contact);
+        $message=(new \Swift_Message('nouveau message'))
+                ->setFrom($contact['email'])
+                ->setTo('contact@senphysiogroup.com')
+                ->setBody(
+                  $this->renderView(
+                    'email/email.html.twig',
+                    compact('contact')
+                  ),
+                  'text/html'
+                )
+        ;
+        $mailer->send($message);
+        echo "Message a ete bien envoye";
+        return $this->redirectToRoute('home');
       }
 
         return $this->render('home/contact.html.twig',['contacForm'=>$form->createView()]);
